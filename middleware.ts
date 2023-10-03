0f51aadd0d61ed13bb54b62bc1ddac0acfe8db65
hephezy <oluwapelumimartins5@gmail.com>
2023-10-03 16:57:23 +0100

    Added webhook to public routes

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,17 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
     publicRoutes: [
-        '/'
+        '/',
+        '/api/webhook',
+        '/question/:id',
+        '/tags',
+        '/tags/:id',
+        '/profile/:id',
+        '/community',
+        '/jobs'
+    ],
+    ignoredRoutes: [
+        '/api/webhook', '/api/chatgpt'
     ]
 });
  
