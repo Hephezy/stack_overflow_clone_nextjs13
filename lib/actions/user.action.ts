843429ef2295a6e67837fd3b0ac79c42c7dcf637
hephezy <oluwapelumimartins5@gmail.com>
2023-10-03 15:49:16 +0100

    fixed errors

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -63,8 +63,8 @@ export async function deleteUser(params: DeleteUserParams) {
       throw new Error('User not found');
      }
 
-     const userQuestionsIds = await Question.find({ author: user._id}).
-     distinct('_id');
+    //  const userQuestionsIds = await Question.find({ author: user._id}).
+    //  distinct('_id');
 
      await Question.deleteMany({ author: user._id});
 
