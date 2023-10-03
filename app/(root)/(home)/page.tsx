76e593e971fba9b287f5c633306c944dceaac8b0
hephezy <oluwapelumimartins5@gmail.com>
2023-10-03 11:28:36 +0100

    Implemented the creation of Questions

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,54 +5,15 @@ import NoResult from "@/components/shared/NoResult";
 import LocalSearchBar from "@/components/shared/Search/LocalSearchBar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filter";
+import { getQuestions } from "@/lib/actions/question.action";
 import Link from "next/link";
 
-const question = [
-  {
-    _id: "1",
-    title: 'How do I use express as a custom server in NextJS',
-    tags: [{ _id: "1", name: 'sql' }, { _id: "2", name: 'python' }],
-    author: {
-      _id: "1",
-      name: 'John Doe',
-      picture: 'john-doe.jpg',
-    },
-    upVotes: 10,
-    views: 100,
-    answers: [],
-    createdAt: new Date('2023-09-01T12:00:00.000Z'),
-  },
-  {
-    _id: "2",
-    title: 'How to perfectly center a Div in Tailwind CSS',
-    tags: [{ _id: "3", name: 'css' }, { _id: "4", name: 'tailwind' }],
-    author: {
-      _id: "2",
-      name: 'Jane Doe',
-      picture: 'jane-doe.jpg',
-    },
-    upVotes: 20,
-    views: 300,
-    answers: [],
-    createdAt: new Date('2023-09-01T12:00:00.000Z'),
-  },
-  {
-    _id: "3",
-    title: 'Redux Toolkit not updating state as expected',
-    tags: [{ _id: "5", name: 'redux' }, { _id: "6", name: 'react' }],
-    author: {
-      _id: "3",
-      name: 'Sarah Doe',
-      picture: 'sarah-doe.jpg',
-    },
-    upVotes: 59,
-    views: 179000,
-    answers: [],
-    createdAt: new Date('2023-09-01T12:00:00.000Z'),
-  }
-];
+export default async function Home() {
 
-export default function Home() {
+  const result = await getQuestions({});
+
+  console.log(result.questions);
+
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -88,8 +49,8 @@ export default function Home() {
       <HomeFilters />
 
       <div className="mt-10 flex w-full flex-col gap-6">
-        {question.length > 0
-          ? question.map((question) => (
+        {result.questions.length > 0
+          ? result.questions.map((question) => (
             <QuestionCard 
               key={question._id}
               _id={question._id}
