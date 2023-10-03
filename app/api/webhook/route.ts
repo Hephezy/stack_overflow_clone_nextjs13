e85a581517d0d7a245f3676292c25c3c4d0308b7
hephezy <oluwapelumimartins5@gmail.com>
2023-10-03 16:24:47 +0100

    Fixed more errors

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,9 +1,9 @@
 /* eslint-disable camelcase */
-import { Webhook } from 'svix'
-import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
-import { createUser, deleteUser, updateUser } from '@/lib/actions/user.action'
-import { NextResponse } from 'next/server'
+import { Webhook } from 'svix';
+import { headers } from 'next/headers';
+import { WebhookEvent } from '@clerk/nextjs/server';
+import { createUser, deleteUser, updateUser } from '@/lib/actions/user.action';
+import { NextResponse } from 'next/server';
  
 export async function POST(req: Request) {
  
